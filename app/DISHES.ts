c35ba30de03677bfc7e19ee89acfc373f3779d0e
foxtrot12 <43786672+foxtrot12@users.noreply.github.com>
2024-02-07 16:22:17 +0530

    Create DISHES.ts

---
+++ app/DISHES.ts
@@ -0,0 +1,194 @@
+export const LunchDishes = [
+  {
+    name: 'Vegetable Pulao with Raita',
+    ingredients: [1, 2, 3, 4],
+  },
+  {
+    name: 'Aloo Gobi (Potato Cauliflower Curry) with Roti',
+    ingredients: [5, 6, 3, 7, 8],
+  },
+  {
+    name: 'Paneer Tikka Masala with Roti',
+    ingredients: [9, 10, 3, 7, 8],
+  },
+  {
+    name: 'Rajma (Kidney Beans Curry) with Jeera Rice',
+    ingredients: [11, 1, 3],
+  },
+  {
+    name: 'Spinach Rice with Yogurt',
+    ingredients: [1, 12, 3, 4],
+  },
+  {
+    name: 'Masoor Dal (Red Lentil Curry) with Roti',
+    ingredients: [13, 3, 7, 8],
+  },
+  {
+    name: 'Paneer Bhurji with Paratha',
+    ingredients: [9, 14, 10, 3, 7, 8],
+  },
+  {
+    name: 'Methi Paratha with Pickle',
+    ingredients: [15, 7, 3, 21],
+  },
+  {
+    name: 'Vegetable Upma',
+    ingredients: [16, 2, 3],
+  },
+  {
+    name: 'Aloo Paratha with Curd',
+    ingredients: [5, 7, 3, 22],
+  },
+  {
+    name: 'Tawa Pulao with Salad',
+    ingredients: [1, 2, 3, 19],
+  },
+  {
+    name: 'Veg Pulao with Raita',
+    ingredients: [1, 2, 3, 4],
+  },
+  {
+    name: 'Mixed Vegetable Curry with Roti',
+    ingredients: [2, 3, 7, 8],
+  },
+  { name: 'Rajma Chawal', ingredients: [11, 1, 3] },
+  {
+    name: 'Vegetable Biryani',
+    ingredients: [1, 2, 3],
+  },
+  {
+    name: 'Chana Masala with Poori',
+    ingredients: [17, 7, 3, 8],
+  },
+  {
+    name: 'Dum Aloo (Potato Curry) with Roti',
+    ingredients: [5, 3, 7, 8],
+  },
+  {
+    name: 'Palak Paratha with Curd',
+    ingredients: [12, 7, 3, 22],
+  },
+  {
+    name: 'Vegetable Fried Rice with Manchurian',
+    ingredients: [1, 2, 3, 20],
+  },
+  {
+    name: 'Bhindi Masala (Okra Curry) with Roti',
+    ingredients: [18, 3, 7, 8],
+  },
+];
+
+export const DinnerDishes = [
+  {
+    name: 'Aloo Gobi (Potato Cauliflower Curry) with Roti',
+    ingredients: [5, 6, 3, 7, 8],
+  },
+  {
+    name: 'Jeera Rice with Mixed Vegetable Curry',
+    ingredients: [1, 2, 3],
+  },
+  {
+    name: 'Palak Paneer (Spinach Paneer Curry) with Roti',
+    ingredients: [12, 9, 3, 7, 8],
+  },
+  {
+    name: 'Aloo Methi (Potato Fenugreek Curry) with Roti',
+    ingredients: [5, 15, 3, 7, 8],
+  },
+  {
+    name: 'Paneer Butter Masala with Roti',
+    ingredients: [9, 10, 3, 7, 7],
+  },
+  {
+    name: 'Vegetable Pulao with Raita',
+    ingredients: [1, 2, 3, 4],
+  },
+  {
+    name: 'Dal Tadka with Jeera Rice',
+    ingredients: [13, 1, 3],
+  },
+  {
+    name: 'Kadhi Pakora with Steamed Rice',
+    ingredients: [27, 22, 3, 1],
+  },
+  {
+    name: 'Vegetable Jalfrezi with Jeera Rice',
+    ingredients: [2, 3, 1],
+  },
+  {
+    name: 'Rajma (Kidney Beans Curry) with Jeera Rice',
+    ingredients: [11, 1, 3],
+  },
+  {
+    name: 'Vegetable Pulao with Raita',
+    ingredients: [1, 2, 3, 4],
+  },
+  {
+    name: 'Mushroom Masala with Roti',
+    ingredients: [24, 3, 7, 8],
+  },
+  {
+    name: 'Veggie Stir-Fry with Steamed Rice',
+    ingredients: [2, 3, 1],
+  },
+  {
+    name: 'Aloo Matar (Potato Pea Curry) with Roti',
+    ingredients: [5, 25, 3, 7, 8],
+  },
+  {
+    name: 'Tomato Rice with Cucumber Raita',
+    ingredients: [1, 10, 3, 26, 4],
+  },
+  {
+    name: 'Paneer Makhani with Naan',
+    ingredients: [9, 10, 3, 7, 7, 23],
+  },
+  {
+    name: 'Spinach Kofta Curry with Rice',
+    ingredients: [12, 27, 3, 1],
+  },
+  {
+    name: 'Dumplings in Spicy Curry (Kofta) with Roti',
+    ingredients: [27, 3, 7, 8],
+  },
+  {
+    name: 'Gajar Matar Sabzi (Carrot Pea Stir-Fry) with Roti',
+    ingredients: [28, 25, 3, 7, 8],
+  },
+  {
+    name: 'Cauliflower Rice with Yogurt',
+    ingredients: [6, 1, 3, 4],
+  },
+];
+
+export const ingredientMap: { [key: number]: string } = {
+  1: 'rice',
+  2: 'mixed_vegetables',
+  3: 'spices',
+  4: 'yogurt',
+  5: 'potatoes',
+  6: 'cauliflower',
+  7: 'flour',
+  8: 'oil',
+  9: 'paneer',
+  10: 'tomatoes',
+  11: 'kidney_beans',
+  12: 'spinach',
+  13: 'red_lentils',
+  14: 'onions',
+  15: 'fenugreek_leaves',
+  16: 'semolina',
+  17: 'chickpeas',
+  18: 'okra',
+  19: 'salad_veggies',
+  20: 'Manchurian_ingredients',
+  21: 'pickle',
+  22: 'curd',
+  23: 'naan_ingredients',
+  24: 'mushrooms',
+  25: 'peas',
+  26: 'cucumber',
+  27: 'gram_flour',
+  28: 'carrots',
+  29: 'cauliflower_rice',
+};
